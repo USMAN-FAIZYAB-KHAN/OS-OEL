ed16df166bc801f0ed37a8a94351ceb94da22e27
Usman Faizyab Khan <usmanfaizyab123@gmail.com>
2025-04-22 14:47:33 +0000

    Initial code added

--- memory-management.c
+++ memory-management.c
@@ -1,1 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+typedef struct 
+{
+    int process_id;
+    int arrival_time;
+} Process;
+
+void print_table(Process *processes, int count) {
+    printf("+------------+-------------------+\n");
+    printf("| Process ID | Arrival Time (ms) |\n");
+    printf("+------------+-------------------+\n");
+
+    for (int i = 0; i < count; i++) {
+        printf("| %-10d | %-17d |\n", processes[i].process_id, processes[i].arrival_time);
+    }
+
+    printf("+------------+-------------------+\n");
+}
+
+void main() {
+    int no_of_process;
+
+    system("clear");
+    printf("Enter number of processes: ");
+    scanf("%d", &no_of_process);
+    
+    Process *processes = (Process *)malloc(sizeof(Process) * 5);
+    if (processes == NULL) {
+        printf("Allocation Failed");
+        exit(0);
+    }
+
+    for (int i = 0; i < no_of_process; i++) {
+        processes[i].process_id = i+1;
+        printf("Enter arrival time of process %d (ms): ", i+1);
+        scanf("%d", &processes[i].arrival_time);
+    }
+
+    system("clear");
+    print_table(processes, no_of_process);
+
+    free(processes);
+}
